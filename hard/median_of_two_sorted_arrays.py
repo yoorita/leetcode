c3c094294151c3b8cdd3a9c7abfba4f8d4d4897f
Rita Lykova <ritaa.lykova@gmail.com>
2024-11-12 18:53:10 +0200

    resolved task Median of two sorted arrays (runtime 3 ms)

--- hard/median_of_two_sorted_arrays.py
+++ hard/median_of_two_sorted_arrays.py
@@ -1,6 +1,11 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-        pass
+    @staticmethod
+    def findMedianSortedArrays(nums1: list[int], nums2: list[int]) -> float:
+        nums = sorted(nums1 + nums2)
+        center = len(nums) // 2
+        if len(nums) % 2 != 0:
+            return float(nums[center])
+        return (nums[center-1] + nums[center]) / 2
 
 if __name__ == '__main__':
-    print(Solution.findMedianSortedArrays())
+    print(Solution.findMedianSortedArrays([1, 2], [3]))
