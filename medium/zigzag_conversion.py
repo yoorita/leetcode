f45dbee9ed3c1309ad825dc3671b9b1adf2cfb16
Rita Lykova <ritaa.lykova@gmail.com>
2024-11-18 10:04:12 +0200

    resolved task "Zigzag conversion"

---
+++ medium/zigzag_conversion.py
@@ -0,0 +1,28 @@
+class Solution:
+    @staticmethod
+    def convert(s: str, numRows: int) -> str:
+        zigzag = [[] for _ in range(numRows)]
+        count = 0
+        flag = True
+        for letter in s:
+            if count < numRows:
+                zigzag[count].append(letter)
+                if flag:
+                    count += 1
+                else:
+                    count -= 1
+
+            if count == numRows:
+                count -= 2
+                flag = False
+
+            if count == 0:
+                flag = True
+
+        return "".join(["".join(i) for i in zigzag])
+
+
+if __name__ == "__main__":
+    print(Solution.convert("PAYPALISHIRING", 3))
+    print(Solution.convert("PAYPALISHIRING", 4))
+    print(Solution.convert("A", 1))
